473ddf2a94927be3407fb1658f53d00297dfd25c
Islam-Rabie2 <islam.rabie25@gmail.com>
2024-01-01 06:25:00 -0800

    first commit

---
+++ src/components/ItemsList.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import Slide from 'react-reveal/Slide';
+const ItemsList = ({ itemsData }) => {
+  return (
+    <Row>
+     <Slide left>
+        {itemsData.length >= 1 ? (
+          itemsData.map((item) => {
+            return (
+              <Col key={item.id} sm="12" className="mb-3">
+                <Card
+                  className="d-flex flex-row"
+                  style={{ backgroundColor: "#F8F8F8" }}
+                >
+                  <Card.Body>
+                    <Card.Title className="d-flex justify-content-between">
+                      <div className="item-title">{item.title}</div>
+                      <div className="item-price">{item.price}</div>
+                    </Card.Title>
+                    <Card.Text className="py-2">
+                      <div className="item-description">{item.description}</div>
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Img
+                    className="img-item"
+                    variant="top"
+                    style={{ width: "350px" }}
+                    src={item.imgUrl}
+                  />
+                </Card>
+              </Col>
+            );
+          })
+        ) : (
+          <h3 className="text-center">لا يوجد اصناف</h3>
+        )}
+      </Slide>
+    </Row>
+  );
+};
+
+export default ItemsList;
